95c0aa1bcbbb91a975021497c1796c196eebd501
Colleen Malone <stitchineer@yahoo.com>
2021-09-14 12:50:32 -0400

    script

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 
+function updateRate(){
+	// get and report value for percentage rate
+	var rateval = document.getElementById("rate").value
+	document.getElementById("rate_val").innerText=rateval+"%";
+
+}
+
+
+
+function compute(){
+	// set variable to principal input
+	var principal = document.getElementById("principal").value;
+	
+	//validation for principal input
+	if(principal <= 0){
+	alert("Please enter a positive value");
+	document.getElementById("principal").focus();
+	return false;
+	}
+	
+	var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+
+	var interest = principal * years * rate /100
+	var year = new Date().getFullYear()+parseInt(years);
+
+
+    p = document.getElementById("principal").value;
+    document.getElementById("interest").innerText=interest;
+    
+  
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,<br>at an interest rate of <mark>"+rate+"</mark>% <br>You will receive an amount of <mark>"+interest+"</mark><br>in the year <mark>"+year+"</mark>";
+
+    
+    }
+
+
+
 function calcGauge(){
 
 var stitches = document.getElementById("A1").value;
